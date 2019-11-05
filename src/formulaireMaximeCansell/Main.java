7b3b32a8099f8b95b470864ac80f82389d7c35d0
Maxime Cansell <maxime.cansell@gmail.com>
2019-11-05 09:59:00 +0100

    apparition Vbox ok

---
+++ src/formulaireMaximeCansell/Main.java
@@ -0,0 +1,13 @@
+package formulaireMaximeCansell;
+
+import View.ViewHandler;
+import javafx.application.Application;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Application.launch((ViewHandler.class));
+    }
+
+
+}
